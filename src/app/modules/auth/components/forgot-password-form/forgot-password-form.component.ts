5f80cf095cd0fb6f31d5f14284483fb3f193a794
bonnier.mamani <bonnier.mamani@lim.upeu.edu.pe>
2024-01-09 12:31:29 -0500

    generate components auth

---
+++ src/app/modules/auth/components/forgot-password-form/forgot-password-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forgot-password-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './forgot-password-form.component.html'
+})
+export class ForgotPasswordFormComponent {
+
+}
---
+++ src/app/modules/auth/components/recovery-form/recovery-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recovery-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './recovery-form.component.html'
+})
+export class RecoveryFormComponent {
+
+}
