637970403029c5425d85e3d729830569bc66d718
bonnier.mamani <bonnier.mamani@lim.upeu.edu.pe>
2024-01-09 12:06:41 -0500

    new config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,29 +1,3 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from "./pages/login/login.component";
-import { BoardsComponent } from "./pages/boards/boards.component";
-import { BoardComponent } from "./pages/board/board.component";
-import { ScrollComponent } from "./pages/scroll/scroll.component";
-import { TableComponent } from "./pages/table/table.component";
 
-export const routes: Routes = [
-  {
-    path: 'login',
-    component: LoginComponent
-  },
-  {
-    path: 'boards',
-    component: BoardsComponent
-  },
-  {
-    path: 'board',
-    component: BoardComponent
-  },
-  {
-    path: 'scroll',
-    component: ScrollComponent
-  },
-  {
-    path: 'table',
-    component: TableComponent
-  }
-];
+export const routes: Routes = [];
