1d917d0fef0ea7704499d3c543a40c2d13565d83
bonnier.mamani <bonnier.mamani@lim.upeu.edu.pe>
2024-01-09 12:43:31 -0500

    complete directory

---
+++ src/app/modules/shared/components/button/button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html'
+})
+export class ButtonComponent {
+
+}
---
+++ src/app/modules/users/pages/user-table/user-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-table.component.html'
+})
+export class UserTableComponent {
+
+}
